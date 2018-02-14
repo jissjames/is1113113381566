1efd99131f141672061d7e96ec1db056093fae77
jissjames <113381566@umail.ucc.ie>
2018-02-14 23:13:50 +0000

    ebus 3

--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -1,3 +1,7 @@
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,6 +9,7 @@
         
         <!--jQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="ebus2_validator.js"></script>
     </head>
     <body>
         <h4>Please enter your payment details</h4>
@@ -23,7 +28,10 @@
             <br/>
             <button onClick="validateDetails()">Validate</button>
             
-
-        <script type="text/javascript" src="ebus2_validator.js"></script>
+            <?php
+            // Set session variables
+            $_SESSION["total"] = $_POST["total"];
+            ?>
+        
     </body>
 </html>
